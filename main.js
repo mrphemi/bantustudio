44d50dae2587975ca7a166bd5947f9050a24d1bd
mrphemi <mrlapinho@gmail.com>
2019-09-06 14:14:56 +0100

    Made changes

--- main.js
+++ main.js
@@ -15,36 +15,65 @@ $(document).ready(function() {
         });
     }
 
-    function isOnScreen(elem) {
-        // if the element doesn't exist, abort
-        if (elem.length == 0) {
-            return;
-        }
-        var $window = jQuery(window);
-        var viewport_top = $window.scrollTop();
-        var viewport_height = $window.height();
-        var viewport_bottom = viewport_top + viewport_height;
-        var $elem = jQuery(elem);
-        var top = $elem.offset().top;
-        var height = $elem.height();
-        var bottom = top + height;
+    // function isOnScreen(elem) {
+    //     // if the element doesn't exist, abort
+    //     if (elem.length == 0) {
+    //         return;
+    //     }
+    //     var $window = jQuery(window);
+    //     var viewport_top = $window.scrollTop();
+    //     var viewport_height = $window.height();
+    //     var viewport_bottom = viewport_top + viewport_height;
+    //     var $elem = jQuery(elem);
+    //     var top = $elem.offset().top;
+    //     var height = $elem.height();
+    //     var bottom = top + height;
 
-        return (
-            (top >= viewport_top && top < viewport_bottom) ||
-            (bottom > viewport_top && bottom <= viewport_bottom) ||
-            (height > viewport_height && top <= viewport_top && bottom >= viewport_bottom)
-        );
-    }
+    //     return (
+    //         (top >= viewport_top && top < viewport_bottom) ||
+    //         (bottom > viewport_top && bottom <= viewport_bottom) ||
+    //         (height > viewport_height && top <= viewport_top && bottom >= viewport_bottom)
+    //     );
+    // }
 
-    function switchNavColor() {
-        if (isOnScreen(jQuery(".darknav"))) {
-            $(".line").addClass("dark");
-            $(".txt").addClass("dark");
-        } else {
-            $(".line").removeClass("dark");
-            $(".txt").removeClass("dark");
-        }
-    }
+    // function switchNavColor() {
+    //     if (isOnScreen(jQuery(".darknav"))) {
+    //         $(".line").addClass("dark");
+    //         $(".txt").addClass("dark");
+    //     } else {
+    //         $(".line").removeClass("dark");
+    //         $(".txt").removeClass("dark");
+    //     }
+    // }
+
+    // function isOffScreen(elem) {
+    //     var $window = jQuery(window);
+    //     var viewport_top = $window.scrollTop();
+    //     var viewport_height = $window.height();
+    //     var viewport_bottom = viewport_top + viewport_height;
+    //     var $elem = jQuery(elem);
+    //     var top = $elem.offset().top;
+    //     var height = $elem.height();
+    //     var bottom = top + height;
+    //     return top <= viewport_top;
+    // }
+
+    // function switchNavColor() {
+    //     var elem = $(".darknav");
+    //     // console.log(isOffScreen(elem));
+    //     if (elem) {
+    //         if (isOffScreen(elem)) {
+    //             $(".line").addClass("dark");
+    //             $(".txt").addClass("dark");
+    //         } else {
+    //             $(".line").removeClass("dark");
+    //             $(".txt").removeClass("dark");
+    //         }
+    //     } else {
+    //         $(".line").addClass("dark");
+    //         $(".txt").addClass("dark");
+    //     }
+    // }
 
     $(".nav_trigger").on("click", openNavigation);
     $(".close_icon").on("click", closeNavigation);
@@ -58,6 +87,4 @@ $(document).ready(function() {
     $(window).resize(function() {
         switchNavColor();
     });
-
-    // TweenLite.to(window, 0.8, {scrollTo: {y:0, autoKill:false}, ease: Power3.easeInOut});
 });
