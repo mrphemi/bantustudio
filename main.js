8642ddbafa8004c71dcd652f8511d5d5cee6217d
mrphemi <mrlapinho@gmail.com>
2019-09-12 14:26:35 +0100

    Made changes

--- main.js
+++ main.js
@@ -15,37 +15,6 @@ $(document).ready(function() {
         });
     }
 
-    // function isOnScreen(elem) {
-    //     // if the element doesn't exist, abort
-    //     if (elem.length == 0) {
-    //         return;
-    //     }
-    //     var $window = jQuery(window);
-    //     var viewport_top = $window.scrollTop();
-    //     var viewport_height = $window.height();
-    //     var viewport_bottom = viewport_top + viewport_height;
-    //     var $elem = jQuery(elem);
-    //     var top = $elem.offset().top;
-    //     var height = $elem.height();
-    //     var bottom = top + height;
-
-    //     return (
-    //         (top >= viewport_top && top < viewport_bottom) ||
-    //         (bottom > viewport_top && bottom <= viewport_bottom) ||
-    //         (height > viewport_height && top <= viewport_top && bottom >= viewport_bottom)
-    //     );
-    // }
-
-    // function switchNavColor() {
-    //     if (isOnScreen(jQuery(".darknav"))) {
-    //         $(".line").addClass("dark");
-    //         $(".txt").addClass("dark");
-    //     } else {
-    //         $(".line").removeClass("dark");
-    //         $(".txt").removeClass("dark");
-    //     }
-    // }
-
     function isOffScreen(elem) {
         if (elem.length == 0) {
             return;
@@ -79,6 +48,7 @@ $(document).ready(function() {
     }
 
     $(".nav_trigger").on("click", openNavigation);
+    $(".full_nav .links a").on("click", closeNavigation);
     $(".close_icon").on("click", closeNavigation);
 
     switchNavColor();
